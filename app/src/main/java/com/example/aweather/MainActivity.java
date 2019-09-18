5c327646d441ad2208c3cf11b4d1912744871a93
IlyaLagutskiy <ilya.laguckiy@yandex.ru>
2019-09-18 20:25:07 +0300

    Added refresh button and dialog

--- app/src/main/java/com/example/aweather/MainActivity.java
+++ app/src/main/java/com/example/aweather/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.aweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.DialogInterface;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refreshButton = findViewById(R.id.refreshButton);
+        refreshButton.setOnClickListener(this);
     }
+
+   @Override
+   public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.refreshButton:
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Важное сообщение!")
+                        .setMessage("Покормите кота!")
+                        .setCancelable(false)
+                        .setNegativeButton("ОК, иду на кухню",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                break;
+        }
+    }
+
+
 }
